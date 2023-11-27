d5b488472d7e3840ae50b4ef729b45f064aaa461
Dominic Galiano <dgaliano@utexas.edu>
2023-11-27 15:30:37 -0500

    Rudimentary login with empty validation

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,11 +1,88 @@
 <script>
-    import { onMount } from 'svelte';
-    import { goto } from '$app/navigation';
-    import { auth } from '../../stores/auth';
-    
+    import { onMount } from "svelte";
+    import { goto } from "$app/navigation";
+    import { auth } from "../../stores/auth";
+
     onMount(() => {
-        return auth.subscribe()
+        auth.subscribe((user) => {
+            if (user) {
+                goto("/");
+            }
+        });
     });
+
+    let email = "";
+    let emailError = false;
+
+    let password = "";
+    let passwordError = false;
+
+    function validateInputs(email, password) {
+        // TODO: implement more validation and error handling
+        if (email.length < 1) {
+            emailError = true;
+            return false;
+        }
+        if (password.length < 1) {
+            passwordError = true;
+            return false;
+        }
+        return true;
+    }
+
+    $: userInputValid = validateInputs(email, password);
 </script>
 
-<h1>Login</h1>
+<div class="wrapper">
+    <main>
+        <h1 class="centered">Login</h1>
+        <form>
+            <label for="email">Email</label>
+            <input
+                type="email"
+                id="email"
+                name="email"
+                placeholder="Email"
+                required
+                aria-invalid={emailError}
+                aria-describedby={emailError ? "email-error" : null}
+                autocomplete="email"
+                bind:value={email}
+            />
+            <label for="password">Password</label>
+            <input
+                type="password"
+                id="password"
+                name="password"
+                placeholder="Password"
+                required
+                aria-invalid={passwordError}
+                aria-describedby={passwordError ? "password-error" : null}
+                autocomplete="new-password"
+                bind:value={password}
+            />
+            <button type="submit" disabled={!userInputValid}>Login</button>
+        </form>
+    </main>
+</div>
+
+<style>
+    .wrapper {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        min-height: 100vh;
+    }
+
+    main {
+        height: fit-content;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    }
+    .centered {
+        text-align: center;
+    }
+    form > * {
+        display: block;
+        margin: 1rem;
+    }
+</style>
