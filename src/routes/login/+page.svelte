4bc4ddf8471b014367370fbf9d29600ceda4790e
Dominic Galiano <dgaliano@utexas.edu>
2023-11-27 14:48:48 -0500

    Redirect on login

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+    import { onMount } from 'svelte';
+    import { goto } from '$app/navigation';
+    import { auth } from '../../stores/auth';
+    
+    onMount(() => {
+        return auth.subscribe()
+    });
+</script>
+
+<h1>Login</h1>
