8d17bff50cf53ef6835650cb0611fb61ab478679
Dominic Galiano <dgaliano@utexas.edu>
2023-11-27 14:43:06 -0500

    Create store for user that is set on authStateChange;

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,13 @@
 <script>
-    import { onMount } from 'svelte';
-    import { initFirebase } from '$lib/client/firebase';
+    import { onMount } from "svelte";
+    import { initFirebase } from "$lib/client/firebase";
+    import { auth as authStore } from "../stores/auth";
+    import { onAuthStateChanged } from "firebase/auth";
 
-    onMount(initFirebase);
+    onMount(() => {
+        const { auth } = initFirebase();
+        onAuthStateChanged(auth, authStore.set);
+    });
 </script>
 
 <slot />
