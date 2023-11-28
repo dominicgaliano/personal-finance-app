88e2a6d7ca347eddaf9d3d718c7280625b54602f
Dominic Galiano <dgaliano@utexas.edu>
2023-11-28 16:00:04 -0500

    Setup emulator mode

--- src/lib/client/firebase.ts
+++ src/lib/client/firebase.ts
@@ -1,7 +1,8 @@
 import { memoize } from "lodash";
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { connectAuthEmulator, getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { browser } from "$app/environment";
 // import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
@@ -14,13 +15,20 @@ const firebaseConfig = {
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  useEmulator: import.meta.env.VITE_FIREBASE_USE_EMULATOR === "true",
 };
 
 // Firebase Initialization Function (memoized for performance)
 export const initFirebase = memoize(() => {
+  if (!browser) {
+    throw new Error("Can't use the Firebase client on the server.");
+  }
   const app = initializeApp(firebaseConfig);
   const auth = getAuth(app);
   const firestore = getFirestore(app);
+  if (firebaseConfig.useEmulator) {
+    connectAuthEmulator(auth, "http://127.0.0.1:9099");
+  }
   // const analytics = getAnalytics(app);
   return { app, auth, firestore };
 });
