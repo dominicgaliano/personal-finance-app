4b6027e1ace0f56b60273c4c83e91ab1e1ce4340
Dominic Galiano <dgaliano@utexas.edu>
2023-11-29 16:27:43 -0500

    Explicitly set firebase env variable in dev

--- src/lib/server/firebase.ts
+++ src/lib/server/firebase.ts
@@ -1,29 +1,36 @@
-import admin from 'firebase-admin';
-import type { DecodedIdToken } from 'firebase-admin/lib/auth/token-verifier';
+import admin from "firebase-admin";
+import type { DecodedIdToken } from "firebase-admin/lib/auth/token-verifier";
 
 // TODO: make this an absolute path
 // TODO: figure out how to use env variable
-import serviceAccount from '../../../key.json';
+import serviceAccount from "../../../key.json";
 
 const initializeFirebase = () => {
-	if (!admin.apps.length) {
-		admin.initializeApp({
-			credential: admin.credential.cert(serviceAccount)
-		});
-	}
+  if (!admin.apps.length) {
+    if (import.meta.env.VITE_FIREBASE_USE_EMULATOR === "true") {
+      console.log('run')
+      process.env["FIREBASE_AUTH_EMULATOR_HOST"] = import.meta.env.VITE_FIREBASE_AUTH_EMULATOR_HOST;
+    }
+
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
+    });
+  }
 };
 
-export async function decodeToken(token: string): Promise<DecodedIdToken | null> {
-	if (!token) {
-		return null;
-	}
+export async function decodeToken(
+  token: string,
+): Promise<DecodedIdToken | null> {
+  if (!token) {
+    return null;
+  }
 
-	try {
-		initializeFirebase();
+  try {
+    initializeFirebase();
 
-		return await admin.auth().verifyIdToken(token);
-	} catch (err) {
-		console.error('An error occurred validating token', (err as Error).message);
-		return null;
-	}
+    return await admin.auth().verifyIdToken(token);
+  } catch (err) {
+    console.error("An error occurred validating token", (err as Error).message);
+    return null;
+  }
 }
