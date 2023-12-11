14ff0cc65fbf45678f24c3c000e2a3a20f528af5
Dominic Galiano <dgaliano@utexas.edu>
2023-12-11 12:06:53 -0500

    Remove merge conflict artifacts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,4 @@
 <script>
-<<<<<<< HEAD
     import DeleteAccountButton from "../components/DeleteAccountButton.svelte";
     import LogoutButton from "../components/LogoutButton.svelte";
     import RedirectGuard from "../components/RedirectGuard.svelte";
@@ -10,14 +9,5 @@
 <p>
     Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
 </p>
-<<<<<<< HEAD
 <LogoutButton />
 <DeleteAccountButton />
-=======
-
-<button on:click={handleClick}>Count: {count}</button>
-
-<p>{count} * 2 = {doubled}</p>
-<p>{doubled} * 2 = {quadrupled}</p>
-
->>>>>>> main
